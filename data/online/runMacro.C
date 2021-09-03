410d3b84c20cb73ad81dd3e93d8ad308737863ae
rhanniga <ryan.p.hannigan@gmail.com>
2021-09-03 00:29:51 -0500

    started big offline notebook

--- data/online/runMacro.C
+++ data/online/runMacro.C
@@ -8,7 +8,7 @@
 void runMacro(bool local=true, bool full=true, bool gridMerge=true){
 
   float MULT_LOW = 0;
-  float MULT_HIGH = 100;
+  float MULT_HIGH = 20;
 
   float TRIG_BIT = AliAODTrack::kIsHybridGCG;
   float ASSOC_BIT = 1024;
@@ -16,15 +16,14 @@ void runMacro(bool local=true, bool full=true, bool gridMerge=true){
   char *CENT_ESTIMATOR = "V0A";
 
   //Starting and ending index of the array containing the run numbers, specifies which range to run over
-  int startIndex = 0;
-  int endIndex = 14;
+  /* int startIndex = 0; */
+  /* int endIndex = 14; */
 
-  // int startIndex = 15;
-  // int endIndex = 28;
+  int startIndex = 15;
+  int endIndex = 28;
 
   TString work_dir = "lambda_hadron_ratio";
-  // TString output_dir = "eff_corr_cent_" + std::to_string(int(MULT_LOW)) + "_" + std::to_string(int(MULT_HIGH));
-  TString output_dir = "filter_bit_comp";
+  TString output_dir = "eff_corr_cent_" + std::to_string(int(MULT_LOW)) + "_" + std::to_string(int(MULT_HIGH)) + "_20210831";
   
   //If we want to download test files from grid then run in one swoop (usually just run completely locally):
   bool gridTest = false;
