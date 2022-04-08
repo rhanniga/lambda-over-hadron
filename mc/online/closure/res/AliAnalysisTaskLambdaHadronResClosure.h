6ec151fe2ef4db1f29998651fcd4f9567df2ee4c
rhanniga <ryan.p.hannigan@gmail.com>
2022-04-08 17:22:14 -0500

    starting to write closure test for res tech

---
+++ mc/online/closure/res/AliAnalysisTaskLambdaHadronResClosure.h
@@ -0,0 +1,133 @@
+/* Copyright(c) 1998-2021, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full copyright notice */
+/*
+                AliAnalysisTaskLambdaHadronResClosure class
+                This is a closure task for the lambda-hadron correlation analysis
+                (can be found in same directory)
+                Origin: Ryan Hannigan, March 2022, ryan.hannigan@cern.ch
+*/
+
+#ifndef AliAnalysisTaskLambdaHadronResClosure_H
+#define AliAnalysisTaskLambdaHadronResClosure_H
+
+// Includes order: standard, ROOT, AliRoot (for objects in this file only)
+#include "TString.h"
+#include "TLorentzVector.h"
+
+#include "AliAnalysisTaskSE.h"
+
+// Forward declaration order: standard, ROOT, AliRoot (for pointers in this file only)
+class TList;
+class TH1D;
+class TH1F;
+class THnSparse;
+class TClonesArray;
+
+class AliAODEvent;
+class AliEventPoolManager;
+class AliPIDResponse;
+class AliMultSelection;
+class AliEventPool;
+class AliAODTrack;
+class AliAODMCParticle;
+class AliAODv0;
+
+
+class AliAnalysisTaskLambdaHadronResClosure : public AliAnalysisTaskSE {
+public:
+  AliAnalysisTaskLambdaHadronResClosure();
+  AliAnalysisTaskLambdaHadronResClosure(const char *name);
+  virtual ~AliAnalysisTaskLambdaHadronResClosure();
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t* option);
+  virtual void Terminate(Option_t* option);
+  void LoadEfficiencies(TString filePath);
+
+  void SetMultBounds(float multLow, float multHigh);
+  void SetTriggerBit(float trigBit);
+  void SetAssociatedBit(float assocBit);
+  void SetCentEstimator(TString estimator);
+
+  struct AliMotherContainer {
+    AliAODv0* vzero;
+    int daughter1ID;
+    int daughter2ID;
+  };
+
+private:
+  float fMultLow; // lower bound for multiplicity
+  float fMultHigh; // upper bound for multiplicity
+  float fDaughterBit; // filter bit for daughter particle
+  float fAssociatedBit; // filter bit for associated particle
+  float fTriggerBit; // filter bit for trigger particle
+
+  TString fCentEstimator;
+
+  AliAODEvent* fAOD; //!>! input event
+  TClonesArray* fMCArray; //!>! input MC stack
+  TList* fOutputList; //!>! output list
+
+  AliEventPoolManager *fCorPoolMgr; //!>! correlation pool manager
+  AliEventPoolManager *fMCCorPoolMgr; //!>! correlation pool manager for MC mixed event
+  
+  TH1D* fTriggerEff; ///> trigger efficiency
+  TH1D* fAssociatedEff; ///> associated efficiency
+  TH1D* fLambdaEff; ///> lambda efficiency
+
+  THnSparse* fTriggerDistEff;  //!>! single particle trigger dist (corrected for efficiency)
+  THnSparse* fTriggerDistEff_checkMC;  //!>! single particle trigger dist (corrected for efficiency)
+  THnSparse* fTriggerDist;  //!>! single particle trigger dist (not corrected for efficiency, is MC hadron, is MC primary)
+  THnSparse* fAssociatedHDist;  //!>! single particle associated hadron dist
+  THnSparse* fAssociatedHDist_checkMC;  //!>! single particle associated hadron dist (is MC hadron, is MC primary)
+  THnSparse* fTriggeredLambdaDist;  //!>! single particle lambda dist within a triggered event
+  THnSparse* fLambdaDist;  //!>! single particle lambda dist
+
+  THnSparse* fTriggerDist_MC;  //!>! single particle trigger dist (MC truth)
+  THnSparse* fAssociatedDist_MC;  //!>! single particle associated hadron dist (MC truth)
+  THnSparse* fLambdaDist_MC;  //!>! single particle lambda dist within a triggered event (MC truth)
+
+  THnSparse* fDphiHLambdaEff;  //!>! hadron-lambda correlation hist (efficiency corrected)
+  THnSparse* fDphiHLambdaEff_MCKin;  //!>! hadron-lambda correlation hist (efficiency corrected, using MC kinematics)
+  THnSparse* fDphiHHEff;   //!>! hadron-hadron correlation hist (efficiency corrected)
+  THnSparse* fDphiHHEff_checkMC;   //!>! hadron-hadron correlation hist (efficiency corrected, trig and assoc are MC primary)
+  THnSparse* fDphiHLambdaMixed; //!>! hadron-lambda mixed correlation hist
+  THnSparse* fDphiHHMixed; //!>! hadron-hadron mixed correlation hist
+
+  THnSparse* fDphiHLambda_MC;  //!>! hadron-lambda correlation hist (MC truth)
+  THnSparse* fDphiHH_MC;   //!>! hadron-hadron correlation hist (MC truth)
+  THnSparse* fDphiHLambdaMixed_MC; //!>! hadron-lambda mixed correlation hist (MC truth)
+  THnSparse* fDphiHHMixed_MC; //!>! hadron-hadron mixed correlation hist (MC truth)
+
+  AliPIDResponse *fpidResponse; //!>!pid response
+  AliMultSelection *fMultSelection; //!>!mult selection
+
+
+  void FillSingleParticleDist(std::vector<AliAODTrack*> particle_list, double zVtx, THnSparse* fDist, bool trig_eff=false);
+  void FillSingleMCParticleDist(std::vector<AliAODMCParticle*> particle_list, double zVtx, THnSparse* fDist);
+  void FillMotherDist(std::vector<AliAnalysisTaskLambdaHadronResClosure::AliMotherContainer> particle_list, float multPercentile, THnSparse* fDist, bool isAntiLambda, bool lambdaEff=true);
+  void FillMCMotherDist(std::vector<AliAODMCParticle*> particle_list, float multPercentile, THnSparse* fDist);
+
+  void MakeSameHLambdaCorrelations(std::vector<AliAODTrack*> trigger_list, std::vector<AliAnalysisTaskLambdaHadronResClosure::AliMotherContainer> lambda_list, THnSparse* fDphi, double zVtx, bool eff, bool isAntiLambda);
+  void MakeSameHLambdaCorrelations_withMCKin(std::vector<AliAODTrack*> trigger_list, std::vector<AliAnalysisTaskLambdaHadronResClosure::AliMotherContainer> lambda_list, THnSparse* fDphi, double zVtx, bool eff);
+  void MakeSameHHCorrelations(std::vector<AliAODTrack*> trigger_list, std::vector<AliAODTrack*> associated_h_list, THnSparse* fDphi, double zVtx, bool eff=true);
+  void MakeMixedHLambdaCorrelations(AliEventPool *fPool, std::vector<AliAnalysisTaskLambdaHadronResClosure::AliMotherContainer> lambda_list, THnSparse* fDphi, double zVtx, bool eff, bool isAntiLambda);
+  void MakeMixedHHCorrelations(AliEventPool *fPool, std::vector<AliAODTrack*> associated_h_list , THnSparse* fDphi, double zVtx, bool eff=true);
+
+  void MakeSameMCHLambdaCorrelations(std::vector<AliAODMCParticle*> trigger_list, std::vector<AliAODMCParticle*> lambda_list, THnSparse* fDphi, double zVtx);
+  void MakeSameMCHHCorrelations(std::vector<AliAODMCParticle*> trigger_list, std::vector<AliAODMCParticle*> associated_h_list, THnSparse* fDphi, double zVtx);
+  void MakeMixedMCHLambdaCorrelations(AliEventPool *fPool, std::vector<AliAODMCParticle*> lambda_list, THnSparse* fDphi, double zVtx);
+  void MakeMixedMCHHCorrelations(AliEventPool *fPool, std::vector<AliAODMCParticle*> associated_h_list , THnSparse* fDphi, double zVtx);
+
+  bool PassDaughterCuts(AliAODTrack *track); // check if the AOD track passes the daughter cuts
+  bool PassTriggerCuts(AliAODTrack *track, bool checkMC = false); // check if the AOD track passes the trigger cuts 
+  bool PassAssociatedCuts(AliAODTrack *track, bool checkMC = false); // check if the AOD track passes the associated cuts
+  uint8_t PassV0LambdaCuts(AliAODv0 *v0); // check if the AOD v0 passes the lambda cuts (0 = no, 1 = yes and lambda, 2 = yes and anti-lambda)
+  bool PassMCTriggerCuts(AliAODMCParticle *mc_track); // check if the MC particle passes the trigger cuts
+  bool PassMCAssociatedCuts(AliAODMCParticle *mc_track); // check if the MC particle passes the associated cuts
+  bool PassMCLambdaCuts(AliAODMCParticle *mc_track); // check if the MC particle passes the lambda cuts 
+  bool IsMCChargedHadron(int pdg_code); // check if the MC particle is a charged hadron
+
+  ClassDef(AliAnalysisTaskLambdaHadronResClosure, 3);
+
+};
+#endif
---
+++ mc/online/closure/res/runMacro.C
@@ -0,0 +1,153 @@
+#include "AliAnalysisTaskLambdaHadronResClosure.h"
+
+void runMacro(bool local=true, bool full=true, bool gridMerge=true){
+
+  float MULT_LOW = 0;
+  float MULT_HIGH = 20;
+
+  float TRIG_BIT = AliAODTrack::kIsHybridGCG;
+  float ASSOC_BIT =  1024; 
+  char *EFF_FILE_PATH = "eff_out.root";
+  char *CENT_ESTIMATOR = "V0A";
+
+  //Starting and ending index of the array containing the run numbers, specifies which range to run over
+  int startIndex = 0; 
+  int endIndex = 28;
+
+  // int startIndex = 15;
+  // int endIndex = 28;
+
+  TString work_dir = "lambda_hadron_resclosure";
+  TString output_dir = "closure_cent_" + std::to_string(int(MULT_LOW)) + "_" + std::to_string(int(MULT_HIGH)) + "_full_stat_run_with_correct_eff";
+  // TString output_dir = "closure_cent_" + std::to_string(int(MULT_LOW)) + "_" + std::to_string(int(MULT_HIGH)) + "_comp_with_eff";
+  
+  //If we want to download test files from grid then run in one swoop (usually just run completely locally):
+  bool gridTest = false;
+  int numTestFiles = 2;
+
+  // So we can access files from the grid (for eff cor and the like)
+  // TGrid::Connect("alien//");
+
+  gInterpreter->ProcessLine(".include $ROOTSYS/include");
+  gInterpreter->ProcessLine(".include $ALICE_ROOT/include");
+
+  AliAnalysisManager *manage = new AliAnalysisManager("");
+  AliAODInputHandler *aodH = new AliAODInputHandler();
+  manage->SetInputEventHandler(aodH);
+
+
+  //MULT SELECTION:
+  gInterpreter->ProcessLine(Form(".x %s", gSystem->ExpandPathName("$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/macros/AddTaskMultSelection.C")));
+
+  //SELECTION TASK:
+  AliPhysicsSelectionTask* physSelTask = reinterpret_cast<AliPhysicsSelectionTask*>(gInterpreter->ProcessLine(Form(".x %s(kFALSE, kTRUE)", gSystem->ExpandPathName("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C"))));
+
+  //PID response:
+  gInterpreter->ProcessLine(Form(".x %s(kFALSE)", gSystem->ExpandPathName("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C")));
+
+  // Generating task object
+  gInterpreter->LoadMacro("AliAnalysisTaskLambdaHadronResClosure.cxx++g");
+  AliAnalysisTaskLambdaHadronResClosure *task = reinterpret_cast<AliAnalysisTaskLambdaHadronResClosure*>(gInterpreter->ProcessLine(Form(".x AddLambdaHadronResClosureTask.C(\"%s\", %f, %f, %f, %f, \"%s\", \"%s\")",
+  "lambdaHadronResClosure",
+  MULT_LOW,
+  MULT_HIGH,
+  TRIG_BIT,
+  ASSOC_BIT,
+  EFF_FILE_PATH,
+  CENT_ESTIMATOR)));
+
+  if(!manage->InitAnalysis()) return;
+  manage->SetDebugLevel(2);
+  manage->PrintStatus();
+  manage->SetUseProgressBar(1, 25);
+
+  if(local) {
+    TChain *chain = new TChain("aodTree");
+    chain->Add("~/Wonderland/native/sim/265525_0001.root");
+    chain->Add("~/Wonderland/native/sim/265525_0002.root");
+    chain->Add("~/Wonderland/native/sim/265525_0003.root");
+    chain->Add("~/Wonderland/native/sim/265525_0004.root");
+    chain->Add("~/Wonderland/native/sim/265525_0005.root");
+    chain->Add("~/Wonderland/native/sim/265525_0006.root");
+    chain->Add("~/Wonderland/native/sim/265525_0007.root");
+    chain->Add("~/Wonderland/native/sim/265525_0008.root");
+    chain->Add("~/Wonderland/native/sim/265525_0009.root");
+    chain->Add("~/Wonderland/native/sim/265525_0010.root");
+    manage->StartAnalysis("local", chain);
+  }
+
+  else {
+    // if we want to run on grid, we create and configure the plugin
+    AliAnalysisAlien *alienHandler = new AliAnalysisAlien();
+    // also specify the include (header) paths on grid
+    alienHandler->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include");
+    // make sure your source files get copied to grid
+    alienHandler->SetAdditionalLibs("AliAnalysisTaskLambdaHadronResClosure.cxx AliAnalysisTaskLambdaHadronResClosure.h");
+    alienHandler->SetAnalysisSource("AliAnalysisTaskLambdaHadronResClosure.cxx");
+    // select the aliphysics version. all other packages
+    // are LOADED AUTOMATICALLY!
+    alienHandler->SetAliPhysicsVersion("vAN-20201026_ROOT6-1");
+    alienHandler->SetAPIVersion("V1.1x");
+    // select the input data
+    alienHandler->SetGridDataDir("//alice/sim/2017/LHC17f2b_fast/");
+    alienHandler->SetDataPattern("/AOD202/*/*AOD.root");
+    // MC has no prefix, data has prefix 000
+    alienHandler->SetRunPrefix("");
+
+
+    // addding runs
+    int runArray[] = {265525, 265521, 265501, 265499, 265435, 265427, 265426, 265425, 265424, 265422, 265421, 265420, 265419, 265388, 265387, 265385, 265384, 265383, 265381, 265378, 265377, 265344, 265343, 265342, 265339, 265338, 265336, 265334, 265332, 265309};
+    int runArrayLength = (int)(sizeof(runArray)/sizeof(runArray[0]));
+
+    if(endIndex > (runArrayLength-1) || endIndex < 0) {
+            std::cout << "Your end index is out of bounds!" << std::endl;
+            std::abort();
+    }
+
+    if(startIndex > (runArrayLength-1) || startIndex < 0) {
+            std::cout << "Your start index is out of bounds!" << std::endl;
+            std::abort();
+    }
+
+    for(int i = startIndex; i < endIndex + 1; i++) {
+     alienHandler->AddRunNumber(runArray[i]);
+    }
+
+    // number of files per subjob
+    alienHandler->SetSplitMaxInputFileNumber(40);
+    alienHandler->SetExecutable("LambdaHadronResClosure.sh");
+    alienHandler->SetJDLName("LambdaHadronResClosure.jdl");
+    alienHandler->SetTTL(30000);
+    alienHandler->SetOutputToRunNo(kTRUE);
+    alienHandler->SetKeepLogs(kTRUE);
+    // merging: run with kTRUE to merge on grid
+    // after re-running the jobs in SetRunMode("terminate")
+    // (see below) mode, set SetMergeViaJDL(kFALSE)
+    // to collect final results
+    alienHandler->SetMaxMergeFiles(15);
+    alienHandler->SetMaxMergeStages(7);
+    alienHandler->SetMergeViaJDL(gridMerge);
+
+    // define the output folders
+    alienHandler->SetGridWorkingDir(work_dir); 
+    alienHandler->SetGridOutputDir(output_dir);
+
+    // connect the alien plugin to the manager
+    manage->SetGridHandler(alienHandler);
+
+    if(gridTest) {
+      alienHandler->SetNtestFiles(numTestFiles);
+      alienHandler->SetRunMode("test");
+      manage->StartAnalysis("grid");
+    }
+    else {
+      if(full) {
+        alienHandler->SetRunMode("full");
+      }
+     else {
+        alienHandler->SetRunMode("terminate");
+     } 
+      manage->StartAnalysis("grid");
+    }
+  }
+}
