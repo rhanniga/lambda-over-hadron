839a35fc6cffbe6752cff958174c8e27bd51e947
rhanniga <ryan.hannigan@cern.ch>
2023-08-30 14:47:36 -0500

    changes for paper

--- data/offline/plotting_scripts/make_mass_plots.py
+++ data/offline/plotting_scripts/make_mass_plots.py
@@ -10,14 +10,14 @@ c.SetBottomMargin(0.12)
 c.SetTicks(1, 1)
 
 for pt_bin in ["central", "low", "high"]:
-    for cent_bin in [0, 20, 50]:
+    for cent_bin in [0, 20, 50, 80]:
 
         if pt_bin == "central":
-            v0_input_file = rt.TFile("output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_20_40_delta_eta_12_normal.root")
+            v0_input_file = rt.TFile("../output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_20_40_delta_eta_12_normal.root")
         elif pt_bin == "low":
-            v0_input_file = rt.TFile("output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_15_25_delta_eta_12_normal_lowpt.root")
+            v0_input_file = rt.TFile("../output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_15_25_delta_eta_12_normal_lowpt.root")
         elif pt_bin == "high":
-            v0_input_file = rt.TFile("output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_25_40_delta_eta_12_normal_highpt.root")
+            v0_input_file = rt.TFile("../output/central_value/v0_avg6_sideband_subtraction_rsb_1135_115_sig_1102_113_trig_40_80_assoc_25_40_delta_eta_12_normal_highpt.root")
         else:
             print("Invalid pt bin")
             quit()
@@ -30,11 +30,15 @@ for pt_bin in ["central", "low", "high"]:
             lambda_mass_dist = v0_input_file.Get("lambda_mass_dist_20_50").Clone("tmp")
             lambda_mass_fit = v0_input_file.Get("lambda_mass_fit_20_50")
             lambda_mass_bg_fit = v0_input_file.Get("bg_fit_20_50")
-        elif cent_bin == 50:
 
+        elif cent_bin == 50:
             lambda_mass_dist = v0_input_file.Get("lambda_mass_dist_50_80").Clone("tmp")
             lambda_mass_fit = v0_input_file.Get("lambda_mass_fit_50_80")
             lambda_mass_bg_fit = v0_input_file.Get("bg_fit_50_80")
+        elif cent_bin == 80:
+            lambda_mass_dist = v0_input_file.Get("lambda_mass_dist_0_80").Clone("tmp")
+            lambda_mass_fit = v0_input_file.Get("lambda_mass_fit_0_80")
+            lambda_mass_bg_fit = v0_input_file.Get("bg_fit_0_80")
         else:
             print("Invalid centrality bin")
             quit()
@@ -66,34 +70,68 @@ for pt_bin in ["central", "low", "high"]:
         lambda_mass_bg_fit.SetLineWidth(2)
         lambda_mass_bg_fit.SetLineStyle(rt.kDashed)
 
+        signal_region_low = 1.102
+        signal_region_high = 1.130
+        lambda_signal_region_box = rt.TBox(signal_region_low, 0, signal_region_high, lambda_mass_dist.GetMaximum())
+        lambda_signal_region_box.SetFillColorAlpha(rt.kGreen + 2, 0.3)
+        lambda_signal_region_box.SetFillStyle(1001)
+        lambda_signal_region_box.SetLineColor(rt.kGreen + 2)
+        lambda_signal_region_box.SetLineWidth(2)
+
+        sideband_region_low = 1.135
+        sideband_region_high = 1.150
+        lambda_sideband_region_box = rt.TBox(sideband_region_low, 0, sideband_region_high, lambda_mass_dist.GetMaximum())
+        lambda_sideband_region_box.SetFillColorAlpha(rt.kOrange + 2, 0.3)
+        lambda_sideband_region_box.SetFillStyle(1001)
+        lambda_sideband_region_box.SetLineColor(rt.kOrange + 2)
+        lambda_sideband_region_box.SetLineWidth(2)
+
+        fit_integral = lambda_mass_fit.Integral(signal_region_low, signal_region_high)
+        bg_integral = lambda_mass_bg_fit.Integral(signal_region_low, signal_region_high)
+        signal_integral = fit_integral - bg_integral
+
+        print(f"Centrality bin: {cent_bin}")
+        print(f"Momentum bin: {pt_bin}")
+        print(f"Signal integral: {signal_integral/0.001}")
+
         lambda_mass_dist.Draw()
         lambda_mass_fit.Draw("SAME")
         lambda_mass_bg_fit.Draw("SAME")
+        lambda_signal_region_box.Draw("SAME")
+        lambda_sideband_region_box.Draw("SAME")
 
-        legend_y_start = 0.5
-        legend_x_start = 0.68
+
+        legend_y_start = 0.3
+        legend_x_start = 0.16
         legend_x_width = 0.2
         legend_y_width = 0.2
         legend_y_finish = legend_y_start + legend_y_width
         legend_x_finish = legend_x_start + legend_x_width
 
         mass_legend = rt.TLegend(legend_x_start, legend_y_start, legend_x_finish, legend_y_finish)
-        mass_legend.SetTextSize(0.043)
+        mass_legend.SetTextSize(0.035)
         mass_legend.SetBorderSize(0)
         mass_legend.SetFillStyle(0)
         mass_legend.AddEntry(lambda_mass_dist, "Data", "lep")
         mass_legend.AddEntry(lambda_mass_fit, "Total fit", "l")
         mass_legend.AddEntry(lambda_mass_bg_fit, "BG fit", "l")
         mass_legend.Draw("SAME")
+
+        region_legend = rt.TLegend(legend_x_start + 0.15, legend_y_start, legend_x_finish + 0.15, legend_y_finish)
+        region_legend.SetTextSize(0.035)
+        region_legend.SetBorderSize(0)
+        region_legend.SetFillStyle(0)
+        region_legend.AddEntry(lambda_signal_region_box, "Sig. reg.", "f")
+        region_legend.AddEntry(lambda_sideband_region_box, "SB reg.", "f")
+        region_legend.Draw("SAME")
             
         label_y_start = 0.9
-        label_x_start = 0.19
-        label_text_space = 0.06
+        label_x_start = 0.17
+        label_text_space = 0.05
         alice_data_label = rt.TLatex()
         alice_data_label.SetNDC()
-        alice_data_label.SetTextSize(0.05)
+        alice_data_label.SetTextSize(0.04)
         alice_data_label.SetTextAlign(13)
-        alice_data_label.DrawLatex(label_x_start, label_y_start, "ALICE Preliminary")
         alice_data_label.DrawLatex(label_x_start, label_y_start - label_text_space, "#bf{p#minusPb #sqrt{#it{s}_{NN}} = 5.02 TeV}")
         if cent_bin == 0:
             alice_data_label.DrawLatex(label_x_start, label_y_start - 2*label_text_space, "#bf{0#minus20% V0A}")
@@ -101,6 +139,8 @@ for pt_bin in ["central", "low", "high"]:
             alice_data_label.DrawLatex(label_x_start, label_y_start - 2*label_text_space, "#bf{20#minus50% V0A}")
         elif cent_bin == 50:
             alice_data_label.DrawLatex(label_x_start, label_y_start - 2*label_text_space, "#bf{50#minus80% V0A}")
+        elif cent_bin == 80:
+            alice_data_label.DrawLatex(label_x_start, label_y_start - 2*label_text_space, "#bf{0#minus80% V0A}")
         alice_data_label.DrawLatex(label_x_start, label_y_start - 3*label_text_space + 0.01, "#bf{#Lambda #rightarrow p#pi^{-} (#bar{#Lambda} #rightarrow #bar{p}#pi^{+})}")
         if pt_bin == "central":
             alice_data_label.DrawLatex(label_x_start, label_y_start - 4*label_text_space, "#bf{2.0 < #it{p}_{T} < 4.0 GeV/#it{c}}")
@@ -118,6 +158,8 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50.eps")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80.eps")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80.eps")
         elif pt_bin == "low":
             if cent_bin == 0:
                 c.SaveAs("figures/lambda_mass_dist_0_20_lowpt.eps")
@@ -125,6 +167,8 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50_lowpt.eps")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80_lowpt.eps")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80_lowpt.eps")
         elif pt_bin == "high":
             if cent_bin == 0:
                 c.SaveAs("figures/lambda_mass_dist_0_20_highpt.eps")
@@ -132,6 +176,8 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50_highpt.eps")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80_highpt.eps")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80_highpt.eps")
 
         if pt_bin == "central":
             if cent_bin == 0:
@@ -140,6 +186,8 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50.pdf")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80.pdf")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80.pdf")
         elif pt_bin == "low":
             if cent_bin == 0:
                 c.SaveAs("figures/lambda_mass_dist_0_20_lowpt.pdf")
@@ -147,6 +195,8 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50_lowpt.pdf")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80_lowpt.pdf")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80_lowpt.pdf")
         elif pt_bin == "high":
             if cent_bin == 0:
                 c.SaveAs("figures/lambda_mass_dist_0_20_highpt.pdf")
@@ -154,3 +204,5 @@ for pt_bin in ["central", "low", "high"]:
                 c.SaveAs("figures/lambda_mass_dist_20_50_highpt.pdf")
             elif cent_bin == 50:
                 c.SaveAs("figures/lambda_mass_dist_50_80_highpt.pdf")
+            elif cent_bin == 80:
+                c.SaveAs("figures/lambda_mass_dist_0_80_highpt.pdf")
